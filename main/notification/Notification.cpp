3ec54457cd64b184d07f6c49461beac20a23260f
DESKTOP-KLGCVKK\Kirill <kirillsoloduhin072@gmail.com>
2026-05-02 19:06:08 +0300

    Добавил команду уведомления

--- main/notification/Notification.cpp
+++ main/notification/Notification.cpp
@@ -18,3 +18,34 @@ Notification::~Notification ()
   // TODO Auto-generated destructor stub
 }
 
+/*
+		gpio_reset_pin(RED_COLOR_PIN);
+		gpio_reset_pin(GREEN_COLOR_PIN);
+		gpio_reset_pin(BLUE_COLOR_PIN);
+
+		gpio_set_direction(RED_COLOR_PIN, GPIO_MODE_OUTPUT);
+		gpio_set_direction(GREEN_COLOR_PIN, GPIO_MODE_OUTPUT);
+		gpio_set_direction(BLUE_COLOR_PIN, GPIO_MODE_OUTPUT);
+		
+		gpio_set_level(RED_COLOR_PIN, 0);
+		gpio_set_level(GREEN_COLOR_PIN, 0);
+		gpio_set_level(BLUE_COLOR_PIN, 0);
+
+		printf("On red\n");
+		gpio_set_level(RED_COLOR_PIN, 1);
+		
+		vTaskDelay(pdMS_TO_TICKS(5000));
+		gpio_set_level(RED_COLOR_PIN, 0);
+
+		printf("On green\n");
+		gpio_set_level(GREEN_COLOR_PIN, 1);
+
+		vTaskDelay(pdMS_TO_TICKS(5000));
+		gpio_set_level(GREEN_COLOR_PIN, 0);
+
+		printf("On blue\n");
+		gpio_set_level(BLUE_COLOR_PIN, 1);
+		
+		vTaskDelay(pdMS_TO_TICKS(5000));
+		gpio_set_level(BLUE_COLOR_PIN, 0);
+*/
---
+++ main/notification/NotificationTypes.hpp
@@ -0,0 +1,34 @@
+/*
+ * NotificationTypes.hpp
+ *
+ *  Created on: 2 мая 2026 г.
+ *      Author: Kirill
+ */
+
+#ifndef MAIN_NOTIFICATION_NOTIFICATIONTYPES_HPP_
+#define MAIN_NOTIFICATION_NOTIFICATIONTYPES_HPP_
+
+#include <stdint.h>
+enum notification_event_type_t
+{
+    SHUTDOWN_NOTIF = 0,
+    INIT = 1,
+    NOTIFICATE = 2
+};
+
+enum notification_source_t
+{
+    LED = 1,
+    NOISE = 2,
+    LED_NOISE = 3
+};
+
+struct notif_cmd_t
+{
+    notification_event_type_t notification_event_type;
+    notification_source_t notif_source;
+    uint8_t duration;
+};
+
+
+#endif /* MAIN_NOTIFICATION_NOTIFICATIONTYPES_HPP_ */
